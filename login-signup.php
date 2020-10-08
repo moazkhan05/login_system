b0217ce0867bbb7b62506336cf9705a1452e47aa
MaazKhan05 <maaz.khan@forrun.co>
2020-10-08 16:15:53 +0500

    scripts from login-signup.php  moved to script.js

--- login-signup.php
+++ login-signup.php
@@ -18,16 +18,6 @@
 require 'actions.php';
 
 session_start();
-//-----------------Login In Click-----------------
- 
-
-
-
-//-----------------Registration Click-----------------
-// if(isset($_POST['btn-register'])){
-//   registration();
-// }
-
 
 // Close connection
 ?>
@@ -233,115 +223,9 @@ session_start();
   <!-- jQuery (Bootstrap JS plugins depend on it) -->
   
   <script src="js/bootstrap.min.js"></script>
-  <!-- <script src="js/ajax-utils.js"></script>-->
-  <script src="js/script.js"></script> 
-  <script>
-    //   funtion to switch forms start
-            $(function() {
-
-            $('#login-form-link').click(function(e) {
-                $('#error-alert').fadeOut(100);
-                $("#login_form").delay(100).fadeIn(100);
-                $("#registration_form").fadeOut(100);
-                $('#register-form-link').removeClass('active');
-                $(this).addClass('active');
-                e.preventDefault();
-            });
-            $('#register-form-link').click(function(e) {
-                $('#error-alert').fadeOut(100);
-                $("#registration_form").delay(100).fadeIn(100);
-                $("#login_form").fadeOut(100);
-                $('#login-form-link').removeClass('active');
-                $(this).addClass('active');
-                e.preventDefault();
-            });
-            });
-    //   function to switch forms end        
-    </script>
-
-    <script >
-    //email validation   
-      $(document).ready(function (){
-        $("#login-btn").click(function(){
-          // var email=$("#input-email").val();
-          // var pwd =$('#input-pwd').val();
-          var data=$("#login_form").serialize();
-          $.ajax({
-                url: 'validation.php',
-                type: "POST",
-                dataType: "json",
-                data : {
-                  // "email" : email,
-                  // "password":pwd,
-                  "data":data,
-                  "email_validation" : "1"
-                },
-                success:function(data) {
-                    console.log(data.errors);
+  <script src="js/ajax-utils.js"></script>
+  <script src="./js/script.js"></script> 
   
-                    $.each( data.errors, function( i, val ) {
-                      $( "#" + i ).text( val );
-                    });
-                    
-                    
-                    if(data.status == 1){
-                      $('#login_form').submit();  
-                    }
-                
-                  },
-                  error : function(){
-                    alert("Request Failed");
-                  }
-               });
-          });
-       });
-    
-    </script>
-
-    <script >   
-      $(document).ready(function (){
-        $("#register-btn").click(function(){
-          // var name=$("#txt-name-register").val();
-          // var email=$("#txt-email-register").val();
-          // var pwd =$('#txt-pass-register').val();
-          // var cnfrm_pwd =$('#txt-confirm-pass-register').val();
-          // var number =$('#txt-phone-register').val();
-          var data=$("#registration_form").serialize();
-          //alert(email);
-          $.ajax({
-                url: 'validation.php',
-                type: "POST",
-                dataType: "json",
-                data : {
-                  // "name" : name,
-                  // "email" : email,
-                  // "password" : pwd,
-                  // "confirm-password" : cnfrm_pwd,
-                  // "phone-number" : number,
-                  "data":data,
-                  "registration-validation" : "1"
-                },
-                success:function(data) {
-                  console.log(data.errors);
-                    console.log(data.status);
-  
-                    $.each( data.errors, function( i, val ) {
-                      $( "#" + i ).text( val );
-                    });
-                    if(data.status == 1){
-                      $('#registration_form').submit();  
-                    }
-                   
-                  },
-                  error : function(){
-                    alert("Request Failed");
-                  }
-               });
-          });
-       });
-    
-    </script>
-
    
 </body>
 </html>
